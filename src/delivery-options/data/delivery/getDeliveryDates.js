ca3672e09527cfe6cdb0fa02fcba5c6af62acd8c
Edie Lemoine <edie@myparcel.nl>
2020-07-30 09:40:48 +0200

    fix: invalid date in safari when selecting delivery date (#9)

--- src/delivery-options/data/delivery/getDeliveryDates.js
+++ src/delivery-options/data/delivery/getDeliveryDates.js
@@ -1,5 +1,6 @@
 import * as CONFIG from '@/data/keys/configKeys';
 import { configBus } from '@/delivery-options/config/configBus';
+import { createDate } from '@/delivery-options/data/dates/createDate';
 import { createIsoString } from '@/delivery-options/data/dates/createIsoString';
 
 /**
@@ -26,17 +27,16 @@ export function getDeliveryDates(deliveryOptions) {
   return deliveryOptions.map((option) => {
     const { date: deliveryMoment } = option;
 
-    const date = new Date(deliveryMoment.date);
     const name = createIsoString(deliveryMoment.date);
-
-    const dateString = date.toLocaleDateString(
-      configBus.get(CONFIG.LOCALE),
-      {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-      },
-    );
+    const dateString = createDate(deliveryMoment.date)
+      .toLocaleDateString(
+        configBus.get(CONFIG.LOCALE),
+        {
+          weekday: 'long',
+          month: 'long',
+          day: 'numeric',
+        },
+      );
 
     const label = dateString.charAt(0).toUpperCase() + dateString.slice(1);
 
