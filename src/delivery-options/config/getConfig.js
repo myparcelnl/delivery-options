ca8a2a872b9dbc6749c09e638b5d6356bbd8f68f
Edie Lemoine <edie@myparcel.nl>
2020-09-17 09:46:15 +0200

    fix: dropoff days are not merged with default values anymore

--- src/delivery-options/config/getConfig.js
+++ src/delivery-options/config/getConfig.js
@@ -1,5 +1,5 @@
-import { CARRIER_SETTINGS } from '@/data/keys/configKeys';
 import { DEFAULT_PLATFORM } from '@/data/keys/settingsConsts';
+import { configurationMerger } from '@/delivery-options/config/configurationMerger';
 import { defaultConfiguration } from '@/config/defaultConfiguration';
 import { getWindowObject } from '@/delivery-options/config/getWindowObject';
 import mergeWith from 'lodash-es/mergeWith';
@@ -16,27 +16,9 @@ export const getConfig = (configuration = getWindowObject()) => {
   const platform = configuration.config ? configuration.config.platform : DEFAULT_PLATFORM;
 
   /**
-   * Customizer function for lodash mergeWith().
-   *
-   * @param {*} defaultVal - The default value.
-   * @param {*} newVal - The new value.
-   * @param {String} key - Key of the current object.
-   *
-   * @returns {*}
-   */
-  const customizer = (defaultVal, newVal, key) => {
-    // If carrier settings is being changed in any way, discard its default value.
-    if (key === CARRIER_SETTINGS) {
-      return newVal;
-    }
-
-    return newVal === null || newVal === '' ? defaultVal : undefined;
-  };
-
-  /**
-   * Merge the config data with the default config. Uses lodash mergeWith to be able to skip undefined entries.
+   * Merge the config data with the default config.
    *
    * @see https://lodash.cobm/docs/4.17.15#mergeWith
    */
-  return mergeWith({}, defaultConfiguration(platform), configuration, customizer);
+  return mergeWith({}, defaultConfiguration(platform), configuration, configurationMerger);
 };
