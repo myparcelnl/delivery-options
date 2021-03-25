436c471d6381a28d159ec6d3973822522b704752
Edie Lemoine <edie@myparcel.nl>
2021-03-25 09:18:12 +0100

    fix: improved error handling

--- src/config/errorConfig.js
+++ src/config/errorConfig.js
@@ -1,20 +1,16 @@
-/* eslint-disable no-unused-vars */
+/* eslint no-multi-spaces: ["warn", { exceptions: { "VariableDeclarator": true } }] */
+
+export const ERROR_MISSING_REQUIRED_PARAMETER = 3212;
+export const ERROR_INVALID_QUERY_PARAMETER    = 3224;
+export const ERROR_INVALID_POSTAL_CODE        = 3505;
+export const ERROR_INVALID_COUNTRY_CODE       = 3506;
+export const ERROR_UNSUPPORTED_CARRIER        = 3728;
 
 /**
- * API error code when any address part is wrong.
+ * Errors that should trigger a "fatal" error, which will hide the delivery options.
  *
- * @type {Number}
+ * @type {Number[]}
  */
-export const ADDRESS_ERROR = 3505;
-
-const ERROR_NO_CC = 3224;
-const ERROR_NO_POSTAL_CODE = 3212;
-const ERROR_INVALID_POSTAL_CODE = 3505;
-// "Country not supported"
-const ERROR_COUNTRY_NOT_SUPPORTED = 3506;
-// be: "delivery_options can only be used in Belgium (cc=BE)"
-// nl: "delivery_options can only be used in the Netherlands or Belgium"
-const ERROR_NL_INVALID_CC = 3212;
-const ERROR_NO_NUMBER = 3212;
-
-const ERROR_INVALID_CARRIER = 3728;
+export const FATAL_ERRORS = [
+  ERROR_INVALID_COUNTRY_CODE,
+];
