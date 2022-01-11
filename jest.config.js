4ce58b5afa76def928c71a241dee092c7d0f31c7
Edie Lemoine <edie@myparcel.nl>
2022-01-11 18:08:19 +0100

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  testTimeout: 10000,
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.js$': 'babel-jest',
-  },
+  // moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  // transform: {
+  //   '^.+\\.vue$': 'vue-jest',
+  //   '^.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+  //   '^.+\\.js$': 'babel-jest',
+  // },
   transformIgnorePatterns: [
     'node_modules/(?!('
-    + 'babel-jest|'
+    // + 'babel-jest|'
     + 'bootstrap-vue|'
-    + 'jest-vue-preprocessor|'
     + 'lodash-es'
     + ')/)',
   ],
@@ -20,25 +18,25 @@ module.exports = {
     '^@Mocks/(.*)$': '<rootDir>/tests/__mocks__/$1',
     '^!?raw-loader!(.*)': '<rootDir>/tests/__mocks__/raw-loader.js',
   },
-  roots: [
-    '<rootDir>/tests',
-  ],
-  snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: [
-    '<rootDir>/tests/unit/**/*.spec.(js|jsx|ts|tsx)',
-  ],
+  // roots: [
+  //   '<rootDir>/tests',
+  // ],
+  // snapshotSerializers: ['jest-serializer-vue'],
+  // testMatch: [
+  //   '<rootDir>/tests/unit/**/*.spec.(js|jsx|ts|tsx)',
+  // ],
   setupFilesAfterEnv: [
-    '@testing-library/jest-dom',
+  //   '@testing-library/jest-dom',
     '<rootDir>/tests/jest-setup',
   ],
-  testURL: 'http://localhost/',
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{js,vue}',
-  ],
-  coverageReporters: ['lcov', 'text-summary'],
+  // testURL: 'http://localhost/',
+  // watchPlugins: [
+  //   'jest-watch-typeahead/filename',
+  //   'jest-watch-typeahead/testname',
+  // ],
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   '<rootDir>/src/**/*.{js,vue}',
+  // ],
+  // coverageReporters: ['lcov', 'text-summary'],
 };
