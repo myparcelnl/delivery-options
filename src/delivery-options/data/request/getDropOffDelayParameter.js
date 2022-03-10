7c737c6d6249f644bea5a807ef5b124531ea736d
markernst97 <markiervc@gmail.com>
2022-03-10 21:33:53 +0100

    fix: add dropoffdelay to nextday deliverymoment

--- src/delivery-options/data/request/getDropOffDelayParameter.js
+++ src/delivery-options/data/request/getDropOffDelayParameter.js
@@ -1,7 +1,7 @@
 import * as CONFIG from '@/data/keys/configKeys';
 import { hasSameDayDelivery } from '@/helpers/delivery/hasSameDayDelivery';
+import { isPastCutoffTime } from '@/helpers/delivery/isPastCutoffTime';
 import { configBus as realConfigBus } from '@/delivery-options/config/configBus';
-import { isPastCutoffTime } from '../../../helpers/delivery/isPastCutoffTime';
 
 /**
  * @param {import('@/delivery-options/config/configBus').configBus} configBus - Optional parameter for easier testing.
@@ -9,8 +9,11 @@ import { isPastCutoffTime } from '../../../helpers/delivery/isPastCutoffTime';
  * @returns {Partial<DeliveryOptionsRequestParameters>}
  */
 export function getDropOffDelayParameter(configBus = realConfigBus) {
-  if (!configBus.isEnabled(CONFIG.ALLOW_SAME_DAY_DELIVERY)) {
-    return {};
+  if (!configBus.isEnabled(CONFIG.ALLOW_SAME_DAY_DELIVERY) && !isPastCutoffTime()) {
+    const dropOffdelay = configBus.get(CONFIG.DROP_OFF_DELAY);
+    return {
+      dropoff_delay: dropOffdelay === 0 ? 1 : dropOffdelay,
+    };
   }
 
   let dropOffdelay = 1;
