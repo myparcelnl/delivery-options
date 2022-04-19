6bd5b57b6fc74d80544672cb024d59cfbc640311
Joeri van Veen <joeri@myparcel.nl>
2022-04-19 15:47:39 +0200

    feat: do not show morning and evening without deliverydate

--- src/delivery-options/data/delivery/dependencies/formatDeliveryMoment.js
+++ src/delivery-options/data/delivery/dependencies/formatDeliveryMoment.js
@@ -1,7 +1,9 @@
+import * as CONFIG from '@/data/keys/configKeys';
 import { SHIPMENT_OPTIONS, deliveryMoments } from '@/config/formConfig';
 import { formatPossibility } from '@/delivery-options/data/delivery/dependencies/formatPossibility';
 import { formatShipmentOptions } from '@/delivery-options/data/delivery/dependencies/formatShipmentOptions';
 import { getDeliveryType } from '@/delivery-options/data/delivery/dependencies/getDeliveryType';
+import { configBus } from '@/delivery-options/config/configBus';
 
 /**
  * Delivery moment is dependant on delivery date.
@@ -15,11 +17,16 @@ export function formatDeliveryMoment(option) {
     .sort((possibilityA, possibilityB) => {
       return deliveryMoments.indexOf(possibilityA.type) - deliveryMoments.indexOf(possibilityB.type);
     })
-    .reduce((acc, possibility) => ({
-      ...acc,
-      [getDeliveryType(possibility)]: {
-        moments: formatPossibility(possibility),
-        [SHIPMENT_OPTIONS]: formatShipmentOptions(possibility),
-      },
-    }), {});
+    .reduce((acc, possibility) => {
+      if (!configBus.get(CONFIG.FEATURE_SHOW_DELIVERY_DATE) && ['morning', 'evening'].includes(possibility.type)) {
+        return { ...acc };
+      }
+      return {
+        ...acc,
+        [getDeliveryType(possibility)]: {
+          moments: formatPossibility(possibility),
+          [SHIPMENT_OPTIONS]: formatShipmentOptions(possibility),
+        },
+      };
+    }, {});
 }
