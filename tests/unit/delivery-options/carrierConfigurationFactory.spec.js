6f1fe5a548b40960d0f13e4caf70fc7d1ea2bc18
Edie Lemoine <edie@myparcel.nl>
2022-10-25 13:40:21 +0200

    test: update coverage

---
+++ tests/unit/delivery-options/carrierConfigurationFactory.spec.js
@@ -0,0 +1,21 @@
+import * as CARRIERS from '@/data/keys/carrierKeys';
+import { CarrierConfigurationFactory } from '@/data/carriers/carrierConfigurationFactory';
+import { MYPARCEL } from '@/data/keys/platformKeys';
+
+describe('CarrierConfigurationFactory', () => {
+  it.each`
+    carrier
+    ${CARRIERS.POSTNL}
+    ${CARRIERS.BPOST}
+    ${CARRIERS.DPD}
+    ${CARRIERS.DHL_FOR_YOU}
+    ${CARRIERS.DPD}
+    ${CARRIERS.INSTABOX}
+    ${CARRIERS.DHL_EUROPLUS}
+    ${CARRIERS.DHL_PARCEL_CONNECT}
+  `('should create a configuration for $carrier', ({ carrier }) => {
+    const config = CarrierConfigurationFactory.create(carrier, MYPARCEL);
+
+    expect(config).toBeDefined();
+  });
+});
