ffd64a96af0df0f67bc0b1a65ea33b6b0b684c97
Edie Lemoine <edie@myparcel.nl>
2022-10-26 10:22:25 +0200

    chore(deps): update dependencies

--- private/createConfig.js
+++ private/createConfig.js
@@ -1,10 +1,9 @@
 const webpack = require('webpack');
 const kebabCase = require('lodash/kebabCase');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 /**
- * @param {String} name - Name.
- * @param {String} slug - Slug to use for folders and such.
+ * @param {string} name - Name.
+ * @param {string} slug - Slug to use for folders and such.
  * @param {webpack.Configuration} config - Config.
  *
  * @returns {webpack.Configuration}
@@ -27,13 +26,6 @@ function createConfig(name, slug = null, config = {}) {
 
     plugins: [
       new webpack.BannerPlugin(`MyParcel ${name}`),
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        defaultSizes: 'parsed',
-        openAnalyzer: false,
-        logLevel: 'info',
-        reportFilename: `analysis/${slug}-report.html`,
-      }),
       ...plugins,
     ],
     ...configObject,
