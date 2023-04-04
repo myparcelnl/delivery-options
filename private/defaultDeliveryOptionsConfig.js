f2fe6459b741b9248709e24967abea5737fb5b4b
Edie Lemoine <edie@myparcel.nl>
2023-04-04 16:27:09 +0200

    build: do not emit any non-js files in the js bundles

--- private/defaultDeliveryOptionsConfig.js
+++ private/defaultDeliveryOptionsConfig.js
@@ -1,3 +1,21 @@
+/**
+ * @type {webpack.Plugin}
+ */
+const onlyEmitJsPlugin = {
+  apply(compiler) {
+    compiler.hooks.emit.tap('DeleteHtmlPlugin', (compilation) => {
+      const { assets } = compilation;
+      const assetNames = Object.keys(assets);
+
+      assetNames.forEach((assetName) => {
+        if (!assetName.endsWith('.js')) {
+          delete assets[assetName];
+        }
+      });
+    });
+  },
+};
+
 const defaultDeliveryOptionsConfig = {
   entry: '@/delivery-options/main.js',
   output: {
@@ -6,6 +24,9 @@ const defaultDeliveryOptionsConfig = {
   optimization: {
     splitChunks: false,
   },
+  plugins: [
+    onlyEmitJsPlugin,
+  ],
 };
 
 module.exports = { defaultDeliveryOptionsConfig };
