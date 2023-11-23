3caa7572774a301c7fa2ac1e0b60559be43f2062
Edie Lemoine <edie@myparcel.nl>
2023-11-23 15:55:46 +0100

    refactor: link translations to form

--- apps/delivery-options/src/form/createDeliveryOptionsForm.ts
+++ apps/delivery-options/src/form/createDeliveryOptionsForm.ts
@@ -1,16 +1,13 @@
-import {FORM_NAME_DELIVERY_OPTIONS, useDeliveryOptionsStore} from '@myparcel-do/shared';
+import {FORM_NAME_DELIVERY_OPTIONS, useDeliveryOptionsStore, useLanguage} from '@myparcel-do/shared';
 import {createForm} from '@myparcel/vue-form-builder';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const createDeliveryOptionsForm = () => {
   const store = useDeliveryOptionsStore();
+  const {translate} = useLanguage();
 
   return createForm(FORM_NAME_DELIVERY_OPTIONS, {
-    renderLabel: (string: string) => {
-      console.log('render label', string);
-      return string;
-    },
-
+    renderLabel: translate,
     initialValues: store.configuration.initial ?? {},
   });
 };
