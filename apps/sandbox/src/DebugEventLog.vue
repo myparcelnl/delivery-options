d49a734203c28f29cd06c0df9337fd8c43cdf0fa
Edie Lemoine <edie@myparcel.nl>
2023-12-07 17:50:13 +0100

    refactor: move table out of legacy folder

--- apps/sandbox/src/DebugEventLog.vue
+++ apps/sandbox/src/DebugEventLog.vue
@@ -6,7 +6,7 @@
       Clear
     </CButton>
 
-    <CTable
+    <SandboxTable
       :content="eventLog.toReversed()"
       :header="['timestamp', 'detail']" />
   </div>
@@ -17,7 +17,7 @@ import {ref} from 'vue';
 import {useEventListener} from '@vueuse/core';
 import {UPDATED_DELIVERY_OPTIONS} from '@myparcel-do/shared';
 import {isOfType} from '@myparcel/ts-utils';
-import CTable from './components/legacy/CTable.vue';
+import SandboxTable from './components/SandboxTable.vue';
 import {CButton} from './components';
 
 const eventLog = ref<string[][]>([]);
