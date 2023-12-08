c03790339b90250f05ac46b0cce93a6c7c22583b
Edie Lemoine <edie@myparcel.nl>
2023-12-08 11:29:29 +0100

    feat(delivery-options): do not expose delivery date if it's turned off

--- apps/delivery-options/src/utils/convertOutput.ts
+++ apps/delivery-options/src/utils/convertOutput.ts
@@ -1,13 +1,16 @@
+import {FEATURE_SHOW_DELIVERY_DATE} from '@myparcel-do/shared';
 import {ShipmentOptionName} from '@myparcel/constants';
 import {type DeliveryOptionsOutput, type InternalOutput, type SelectedDeliveryMoment} from '../types';
+import {getResolvedValue} from './getResolvedValue';
 
 export const convertOutput = (output: InternalOutput): DeliveryOptionsOutput => {
   const deliveryMoment = JSON.parse(output.deliveryMoment || '{}') as SelectedDeliveryMoment;
+  const showDeliveryDate = getResolvedValue(FEATURE_SHOW_DELIVERY_DATE);
 
   return {
     deliveryType: deliveryMoment.deliveryType,
     packageType: deliveryMoment.packageType,
-    date: output.deliveryDate,
+    date: showDeliveryDate ? output.deliveryDate : undefined,
     carrier: deliveryMoment.carrier,
     isPickup: false,
     shipmentOptions: {
