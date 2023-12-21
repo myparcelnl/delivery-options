c4d0ed21b7a110d6b5112d262c657cf3af9e5e91
Edie Lemoine <edie@myparcel.nl>
2023-12-21 16:46:39 +0100

    build: use tailwind in histoire

--- apps/delivery-options/histoire.setup.ts
+++ apps/delivery-options/histoire.setup.ts
@@ -1,5 +1,6 @@
 import {createPinia} from 'pinia';
 import {defineSetupVue3} from '@histoire/plugin-vue';
+import './src/assets/index.scss';
 
 export const setupVue3 = defineSetupVue3(({app}) => {
   app.use(createPinia());
