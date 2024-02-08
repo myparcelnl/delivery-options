428768b5fb974e78c4f771f8d549fac9e94e5ca6
Edie Lemoine <edie@myparcel.nl>
2024-02-08 13:49:19 +0100

    build(release): set npm token in prepare step

--- libs/semantic-release-plugin/src/prepare/index.ts
+++ libs/semantic-release-plugin/src/prepare/index.ts
@@ -1,8 +1,10 @@
 import {type PrepareCmd} from '../types';
 import {updatePackageJsonVersions} from './updatePackageJsonVersions';
+import {setNpmAuth} from './setNpmAuth';
 import {gitCommit} from './gitCommit';
 
 export const prepare: PrepareCmd = async (_, context) => {
+  await setNpmAuth(context);
   await updatePackageJsonVersions(context);
   await gitCommit(context);
 };
