dd49737c6daa716cad0892ee744d68742f12ec05
Edie Lemoine <edie@myparcel.nl>
2024-02-21 17:02:24 +0100

    refactor: increase coverage by removing unreachable code

--- libs/shared/src/composables/form/generateFieldId.ts
+++ libs/shared/src/composables/form/generateFieldId.ts
@@ -1,12 +1,10 @@
-import {unref} from 'vue';
+import {toValue} from 'vue';
 import {type MaybeRef} from '@vueuse/core';
-import {useElement} from '@myparcel/vue-form-builder';
 import {type ElementInstance} from '../../types';
 
-export const generateFieldId = (element?: MaybeRef<ElementInstance>): string => {
-  const resolvedElement = unref(element) ?? useElement();
-
+export const generateFieldId = (element: MaybeRef<ElementInstance>): string => {
+  const resolvedElement = toValue(element);
   const formBaseName = resolvedElement.form.name.split('--')[0];
 
-  return `${formBaseName}--${resolvedElement.name ?? '?'}`;
+  return `${formBaseName}--${resolvedElement.name}`;
 };
