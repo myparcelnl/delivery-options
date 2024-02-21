5db8005719970fff497187c726198319befe8d16
Edie Lemoine <edie@myparcel.nl>
2024-02-21 15:14:19 +0100

    fix(strings): fix delivery and package type translations

--- apps/delivery-options/src/utils/language/createDeliveryTypeTranslatable.ts
+++ apps/delivery-options/src/utils/language/createDeliveryTypeTranslatable.ts
@@ -1,7 +1,8 @@
+import {camel} from 'radash';
 import {type Translatable} from '@myparcel-do/shared';
 import {type DeliveryTypeName} from '@myparcel/constants';
 import {createTranslatable} from './createTranslatable';
 
 export const createDeliveryTypeTranslatable = (deliveryType: DeliveryTypeName): Translatable => {
-  return createTranslatable(`delivery_type_${deliveryType}`);
+  return createTranslatable(camel(`delivery_${deliveryType}_title`));
 };
--- apps/delivery-options/src/utils/language/createPackageTypeTranslatable.ts
+++ apps/delivery-options/src/utils/language/createPackageTypeTranslatable.ts
@@ -1,7 +1,8 @@
+import {camel} from 'radash';
 import {type Translatable} from '@myparcel-do/shared';
 import {type PackageTypeName} from '@myparcel/constants';
 import {createTranslatable} from './createTranslatable';
 
 export const createPackageTypeTranslatable = (packageType: PackageTypeName): Translatable => {
-  return createTranslatable(`package_type_${packageType}`);
+  return createTranslatable(camel(`package_type_${packageType}`));
 };
