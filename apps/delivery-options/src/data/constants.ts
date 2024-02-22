b8c66b9ccdffa33832f6c484c1ef87d29bd2b605
Edie Lemoine <edie@myparcel.nl>
2024-02-22 13:20:46 +0100

    refactor: add comments to constants

--- apps/delivery-options/src/data/constants.ts
+++ apps/delivery-options/src/data/constants.ts
@@ -1,17 +1,32 @@
 import {ShipmentOptionName} from '@myparcel/constants';
 
+/**
+ * Stores whether home delivery or pickup is selected.
+ */
 export const FIELD_HOME_OR_PICKUP = 'homeOrPickup';
 
 export const HOME_OR_PICKUP_HOME = 'home';
 
-export const FIELD_DELIVERY_MOMENT = 'deliveryMoment';
-
-export const FIELD_DELIVERY_DATE = 'deliveryDate';
-
-export const FIELD_SHIPMENT_OPTIONS = 'shipmentOptions';
-
 export const HOME_OR_PICKUP_PICKUP = 'pickup';
 
+/**
+ * Stores the selected delivery date for home delivery.
+ */
+export const FIELD_DELIVERY_DATE = 'deliveryDate';
+
+/**
+ * Stores the selected delivery moment for home delivery.
+ */
+export const FIELD_DELIVERY_MOMENT = 'deliveryMoment';
+
+/**
+ * Stores the selected shipment options for home delivery.
+ */
+export const FIELD_SHIPMENT_OPTIONS = 'shipmentOptions';
+
+/**
+ * Stores the selected pickup location if pickup is selected.
+ */
 export const FIELD_PICKUP_LOCATION = 'pickupLocation';
 
 export const SHOWN_SHIPMENT_OPTIONS = [ShipmentOptionName.Signature, ShipmentOptionName.OnlyRecipient] as const;
