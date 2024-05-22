c0dce76ef599c096f128be9e5cffdedad4e43ca0
Florian Riecker- de Vries <93703843+FlorianSDV@users.noreply.github.com>
2024-05-22 11:22:06 +0200

    feat: add mailbox to dpd (#225)
    INT-494

--- libs/shared/src/config/getMyParcelConfig.ts
+++ libs/shared/src/config/getMyParcelConfig.ts
@@ -188,7 +188,7 @@ export const getMyParcelConfig = (): PlatformConfiguration => ({
     {
       name: CarrierName.Dpd,
       subscription: SubscriptionType.Required,
-      packageTypes: [PackageTypeName.Package],
+      packageTypes: [PackageTypeName.Package, PackageTypeName.Mailbox],
       deliveryTypes: [DeliveryTypeName.Standard, DeliveryTypeName.Pickup],
       deliveryCountries: [
         AUSTRIA,
