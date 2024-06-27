4b075afa21126f8d19b8b6d8ec1f2190048d50fb
Edie Lemoine <edie@myparcel.nl>
2024-06-27 16:39:32 +0200

    test: cover this in tests

--- apps/delivery-options/src/composables/useResolvedDeliveryOptions.spec.ts
+++ apps/delivery-options/src/composables/useResolvedDeliveryOptions.spec.ts
@@ -1,8 +1,17 @@
 import {describe, it, expect, beforeEach} from 'vitest';
+import {assign} from 'radash';
 import {setActivePinia, createPinia} from 'pinia';
 import {normalizeDate} from '@vueuse/core';
 import {mockGetDeliveryOptions} from '@myparcel-do/shared/testing';
-import {KEY_CONFIG, CarrierSetting, KEY_CARRIER_SETTINGS, createTimestamp} from '@myparcel-do/shared';
+import {
+  KEY_CONFIG,
+  CarrierSetting,
+  KEY_CARRIER_SETTINGS,
+  createTimestamp,
+  type InputDeliveryOptionsConfiguration,
+  KEY_ADDRESS,
+} from '@myparcel-do/shared';
+import {type RecursivePartial} from '@myparcel/ts-utils';
 import {DeliveryTypeName, CarrierName} from '@myparcel/constants';
 import {
   waitForDeliveryOptions,
@@ -12,22 +21,49 @@ import {
 } from '../__tests__';
 import {useResolvedDeliveryOptions} from './useResolvedDeliveryOptions';
 
-const mockSetup = async (): Promise<void> => {
+const CARRIER_IDENTIFIER_WITH_CONTRACT = `${CarrierName.PostNl}:1234`;
+
+const doTestSetup = async (config: RecursivePartial<InputDeliveryOptionsConfiguration> = {}): Promise<void> => {
+  const morning = normalizeDate('2022-01-01T09:00:00');
+  const standard = normalizeDate('2022-01-01T15:00:00');
+  const evening = normalizeDate('2022-01-01T20:00:00');
+
+  mockGetDeliveryOptions.mockReturnValueOnce(
+    Promise.resolve([
+      {
+        date: createTimestamp(standard),
+        possibilities: [
+          createDeliveryPossibility(evening, {type: DeliveryTypeName.Evening}),
+          createDeliveryPossibility(standard),
+          createDeliveryPossibility(morning, {type: DeliveryTypeName.Morning}),
+        ],
+      },
+    ]),
+  );
+
+  const carrierSettings = {
+    [CarrierSetting.AllowDeliveryOptions]: true,
+    [CarrierSetting.AllowMondayDelivery]: true,
+    [CarrierSetting.AllowSameDayDelivery]: true,
+    [CarrierSetting.AllowEveningDelivery]: true,
+    [CarrierSetting.AllowMorningDelivery]: true,
+    [CarrierSetting.AllowStandardDelivery]: true,
+  };
+
   mockDeliveryOptionsConfig(
-    getMockDeliveryOptionsConfiguration({
-      [KEY_CONFIG]: {
-        [KEY_CARRIER_SETTINGS]: {
-          [CarrierName.PostNl]: {
-            [CarrierSetting.AllowDeliveryOptions]: true,
-            [CarrierSetting.AllowMondayDelivery]: true,
-            [CarrierSetting.AllowSameDayDelivery]: true,
-            [CarrierSetting.AllowEveningDelivery]: true,
-            [CarrierSetting.AllowMorningDelivery]: true,
-            [CarrierSetting.AllowStandardDelivery]: true,
+    getMockDeliveryOptionsConfiguration(
+      assign(
+        {
+          [KEY_CONFIG]: {
+            [KEY_CARRIER_SETTINGS]: {
+              [CarrierName.PostNl]: carrierSettings,
+              [CARRIER_IDENTIFIER_WITH_CONTRACT]: carrierSettings,
+            },
           },
         },
-      },
-    }),
+        config,
+      ),
+    ),
   );
 
   await waitForDeliveryOptions();
@@ -35,35 +71,38 @@ const mockSetup = async (): Promise<void> => {
 
 describe('useResolvedDeliveryOptions', () => {
   beforeEach(() => {
+    useResolvedDeliveryOptions.clear();
     setActivePinia(createPinia());
   });
 
   it('sorts options by time', async () => {
-    const morning = normalizeDate('2022-01-01T09:00:00');
-    const standard = normalizeDate('2022-01-01T15:00:00');
-    const evening = normalizeDate('2022-01-01T20:00:00');
-
-    mockGetDeliveryOptions.mockReturnValueOnce(
-      Promise.resolve([
-        {
-          date: createTimestamp(standard),
-          possibilities: [
-            createDeliveryPossibility(evening, {type: DeliveryTypeName.Evening}),
-            createDeliveryPossibility(standard),
-            createDeliveryPossibility(morning, {type: DeliveryTypeName.Morning}),
-          ],
-        },
-      ]),
-    );
-
-    await mockSetup();
+    await doTestSetup();
 
     const options = useResolvedDeliveryOptions();
 
-    expect(options.value.map((option) => option.deliveryType)).toEqual([
-      DeliveryTypeName.Morning,
-      DeliveryTypeName.Standard,
-      DeliveryTypeName.Evening,
+    const resolvedOptions = options.value.map(({carrier, deliveryType}) => ({carrier, deliveryType}));
+
+    expect(resolvedOptions).toEqual([
+      {carrier: CarrierName.PostNl, deliveryType: DeliveryTypeName.Morning},
+      {carrier: CarrierName.PostNl, deliveryType: DeliveryTypeName.Standard},
+      {carrier: CarrierName.PostNl, deliveryType: DeliveryTypeName.Evening},
+      {carrier: CARRIER_IDENTIFIER_WITH_CONTRACT, deliveryType: DeliveryTypeName.Morning},
+      {carrier: CARRIER_IDENTIFIER_WITH_CONTRACT, deliveryType: DeliveryTypeName.Standard},
+      {carrier: CARRIER_IDENTIFIER_WITH_CONTRACT, deliveryType: DeliveryTypeName.Evening},
+    ]);
+  });
+
+  it('handles fake delivery', async () => {
+    // DE is not a delivery country for PostNL.
+    await doTestSetup({[KEY_ADDRESS]: {cc: 'DE'}});
+
+    const options = useResolvedDeliveryOptions();
+
+    const resolvedOptions = options.value.map(({carrier, deliveryType}) => ({carrier, deliveryType}));
+
+    expect(resolvedOptions).toEqual([
+      {carrier: CarrierName.PostNl, deliveryType: DeliveryTypeName.Standard},
+      {carrier: CARRIER_IDENTIFIER_WITH_CONTRACT, deliveryType: DeliveryTypeName.Standard},
     ]);
   });
 });
