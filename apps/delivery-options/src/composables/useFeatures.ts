fbd1c437a70ffbc188e23d00fd48cc298ac36429
Clerise <clerise.swart@myparcel.nl>
2024-12-12 12:30:55 +0200

    fix: select delivery date for small package type (#263)
    INT-752
    
    ---------
    
    Co-authored-by: Clerise Swart <clerise.swart@sterrk.nl>

--- apps/delivery-options/src/composables/useFeatures.ts
+++ apps/delivery-options/src/composables/useFeatures.ts
@@ -1,19 +1,22 @@
 import {computed} from 'vue';
 import {useMemoize} from '@vueuse/core';
-import {ConfigSetting, PACKAGE_TYPE_DEFAULT} from '@myparcel-do/shared';
+import {ConfigSetting} from '@myparcel-do/shared';
+import {PackageTypeName} from '@myparcel/constants';
 import {useConfigStore} from '../stores';
 import {SHOWN_SHIPMENT_OPTIONS} from '../data';
+
+const SUPPORTED_PACKAGE_TYPES = [PackageTypeName.Package, PackageTypeName.PackageSmall];
 
 export const useFeatures = useMemoize(() => {
   const config = useConfigStore();
 
   return {
     availableShipmentOptions: computed(() => {
-      return PACKAGE_TYPE_DEFAULT === config.packageType ? SHOWN_SHIPMENT_OPTIONS : [];
+      return SUPPORTED_PACKAGE_TYPES.includes(config.packageType) ? SHOWN_SHIPMENT_OPTIONS : [];
     }),
 
     showDeliveryDate: computed(() => {
-      return PACKAGE_TYPE_DEFAULT === config.packageType && config[ConfigSetting.ShowDeliveryDate];
+      return SUPPORTED_PACKAGE_TYPES.includes(config.packageType) && config[ConfigSetting.ShowDeliveryDate];
     }),
   };
 });
