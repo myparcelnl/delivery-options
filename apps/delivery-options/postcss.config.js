6643911f1d3bcb340f3b1fd503a356d30226cb8a
Remco Horters <remco.horters@myparcel.nl>
2025-05-19 16:47:48 +0200

    feat: replace tailwind variables

--- apps/delivery-options/postcss.config.js
+++ apps/delivery-options/postcss.config.js
@@ -2,6 +2,13 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-replace': {
+      pattern: /(--tw|\*, ::before, ::after)/g,
+      data: {
+        '--tw': '--mp-tw', // Prefixing
+        '*, ::before, ::after': ':root', // So variables does not pollute every element
+      },
+    },
     ...(process.env.NODE_ENV === 'production' ? {cssnano: {}} : {}),
   },
 };
