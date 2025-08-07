0906b3d7dcf293007deec2942b529caca3abb96a
Simon van Kouteren <simon.vankouteren@myparcel.nl>
2025-08-07 15:32:15 +0200

    fix: remove unwanted env changes

--- libs/shared/src/env.d.ts
+++ libs/shared/src/env.d.ts
@@ -6,11 +6,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-
-declare global {
-  interface Window {
-    MyParcelConfig?: {
-      platformConfig?: any;
-    };
-  }
-}
