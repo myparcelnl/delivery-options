86a7584f83d034931d851fd5a206a48d39663a60
Nabil Azahaf <nabil.azahaf@myparcel.nl>
2025-12-15 12:55:18 +0100

    fix: add CARRIER_INPOST mock data for new carrier in constants

--- libs/shared/src/__tests__/mocks/fakeCarriersResponse.ts
+++ libs/shared/src/__tests__/mocks/fakeCarriersResponse.ts
@@ -11,6 +11,7 @@ import {
   CARRIER_DHL_PARCEL_CONNECT,
   CARRIER_DPD,
   CARRIER_INSTABOX,
+  CARRIER_INPOST,
   CARRIER_POST_NL,
   CARRIER_UPS_STANDARD,
   CARRIER_UPS_EXPRESS_SAVER,
@@ -36,6 +37,7 @@ const allCarrierData = [
   CARRIER_GLS,
   CARRIER_BRT,
   CARRIER_TRUNKRS,
+  CARRIER_INPOST,
 ] satisfies Carrier[];
 
 export const fakeCarriersResponse = (carrier?: CarrierNameOrId): Carrier[] => {
