a10ae83090f6a8c949344a5e487cffbb29516441
Joeri van Veen <joeri@myparcel.nl>
2026-03-24 15:26:58 +0100

    wip

---
+++ apps/sandbox/src/components/SandboxApiKeyBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <Box class="mp-gap-4 mp-grid">
+    <h2>API key</h2>
+    <FormTextInput
+      v-model="apiKey"
+      class="mp-w-full" />
+  </Box>
+</template>
+
+<script lang="ts" setup>
+import {computed} from 'vue';
+import {useSandboxStore} from '../stores';
+import FormTextInput from './form/input/FormTextInput.vue';
+import {Box} from './Box';
+
+const sandboxStore = useSandboxStore();
+
+const apiKey = computed({
+  get: () => sandboxStore.config.apiKey ?? '',
+  set: (value) => {
+    sandboxStore.config.apiKey = value || undefined;
+  },
+});
+</script>
