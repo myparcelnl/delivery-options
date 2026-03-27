2d68178b66874405142f33a7624287c40035b8a8
Joeri van Veen <joeri@myparcel.nl>
2026-03-27 14:46:16 +0100

    fix: show price <= 0 as free, but really now

--- apps/delivery-options/src/components/common/PriceTag/PriceTag.vue
+++ apps/delivery-options/src/components/common/PriceTag/PriceTag.vue
@@ -11,15 +11,15 @@ const {state: config} = useConfigStore();
 const {translate} = useLanguage();
 
 const formattedPrice = computed(() => {
+  if (props.price <= 0 && config[ConfigSetting.ShowPriceZeroAsFree]) {
+    return translate('free');
+  }
+
   if (props.price !== 0) {
     return props.price.toLocaleString('nl-NL', {
       style: 'currency',
       currency: 'EUR',
     });
-  }
-
-  if (config[ConfigSetting.ShowPriceZeroAsFree]) {
-    return translate('free');
   }
 
   return null;
